fedbc4638e68386bda8be06b3c97c90036f0ddf5
Travis S <travis@icraft.io>
2024-12-17 13:44:11 -0500

    Removed Demo Files

--- src/app/reports/page.tsx
+++
@@ -1,9 +0,0 @@
-import QAReportForm from '@/components/qa/QAApp';
-
-export default function ReportsPage() {
-  return (
-    <main className="container mx-auto py-8">
-      <QAReportForm />
-    </main>
-  );
-}
